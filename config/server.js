e0922b66183e51d0d0ee94681af470cb1ba498e6
Augusto Mallmann <augustocmallmann@gmail.com>
2021-02-15 16:01:53 -0300

    deploy

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0508ba342853a437c50215e28de428f9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '654ac547-7f96-4bb8-9b6f-3af6c0beb924'
+};
